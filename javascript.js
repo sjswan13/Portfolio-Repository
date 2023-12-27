54a1640045d98beb2e60c4ad96394b911636be2c
sjswan13 <sjswan13@gmail.com>
2023-12-26 18:53:46 -0700

    js, etc

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
 console.log("Hello World!");
+
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+console.log(form, submitButton)
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  console.log ('poop')
+  window.location.href = 'success.html'
+})
